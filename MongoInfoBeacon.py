2bd11ad318b132662dc64393658eee877fecb603
trebryan <treb@hrbr.io>
2018-12-05 20:10:20 -0800

    changed the connection to the produciton servers

--- MongoInfoBeacon.py
+++ MongoInfoBeacon.py
@@ -59,7 +59,7 @@ while True:
 
     body = list_members()
 
-    myurl = "https://harbor-stream-staging.herokuapp.com/beacon"
+    myurl = "https://harbor-stream.herokuapp.com/beacon"
     headers = {
         'Content-Type': 'application/json',
         'apiKey': apiKey,
